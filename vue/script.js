a1364f071f1055b74d5f36648e3126df66a3212a
Edomak <edoardo.maccherini@gmail.com>
2021-10-13 17:48:22 +0200

    just for fun

--- vue/script.js
+++ vue/script.js
@@ -1,6 +1,30 @@
 var app = new Vue({
     el: '#app',
     data: {
-      
+        quotes_api_url: "https://type.fit/api/quotes",
+        quotes: [],
+        currentQuote: "",
+        currentAuthor: ""
+    },
+    mounted: function() {
+        axios
+        .get(this.quotes_api_url)
+        .then(
+            (result) => {
+                // console.log(result.data);
+                this.quotes = result.data
+                //  console.log(this.quotes);
+                this.getRanQuote();
+            }
+        )
+
+    },
+    methods: {
+        getRanQuote: function () {
+            var q = Math.floor(Math.random() * this.quotes.length);
+
+            this.currentQuote = this.quotes[q].text;
+            this.currentAuthor = this.quotes[q].author;
+        }
     }
-  })
+})
